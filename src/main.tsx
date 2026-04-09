302044e935490e1df565c9045332eb11407387ea
RogerioSacanucci <rogeriosacanucci02@gmail.com>
2026-04-09 16:59:37 -0300

    adjust main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// Suppress known lottie-web bug: destroy() called on elements that lack the method
+// https://github.com/airbnb/lottie-web/issues/2198
+const _consoleError = console.error.bind(console);
+console.error = (...args: unknown[]) => {
+  if (typeof args[0] === 'string' && args[0].includes('destroy is not a function')) return;
+  _consoleError(...args);
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
